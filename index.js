833a708b7bd729fc8a1ba39e431c832bf21d670e
razumolla <rajuahmed9344@gmail.com>
2022-05-27 20:01:43 +0600

    get one data by Id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -29,6 +29,12 @@ async function run() {
       res.send(tools);
     })
 
+    app.get('/tools/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
+      const tool = await toolCollection.findOne(query);
+      res.send(tool)
+    })
 
 
   } finally {
